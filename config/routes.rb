1e7d1ef5a1637311424028808371d943b2cefdab
Jonathan Perry <jpper@umich.edu>
2023-12-12 15:12:05 -0600

    Implement some comments with error

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
 
   root "posts#index"
 end
